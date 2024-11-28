de81161af89b61edb05c1ad4871923f507904b5b
Iamsheye <seyepelumi@gmail.com>
2024-11-28 14:44:26 +0100

    router & dependency installation

---
+++ src/routes/__root.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Outlet, ScrollRestoration, createRootRoute } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      <ScrollRestoration />
+      <Outlet />
+    </React.Fragment>
+  );
+}
