a35910eb9f5cc7642a95747bb3d76e04b00a27ff
Iamsheye <seyepelumi@gmail.com>
2024-12-04 15:14:58 +0100

    added shadcnui

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
