1e931bffcc65eb7c32d3f53db0bf8e3ee8c92936
Iamsheye <seyepelumi@gmail.com>
2024-12-07 21:33:35 +0100

    increase debounce delay to 900ms

--- src/routes/_auth_routes/dashboard.tsx
+++ src/routes/_auth_routes/dashboard.tsx
@@ -42,7 +42,7 @@ function Dashboard({ isArchived }: { isArchived: boolean }) {
     reasons: [],
   });
 
-  const debouncedSearch = useDebounce(search, 750);
+  const debouncedSearch = useDebounce(search, 900);
 
   const { data, fetchNextPage, isLoading, isError, isFetchingNextPage } =
     useInfiniteQuery({
